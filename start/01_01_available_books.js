e4468cf1449d7adeb3265d6a75d4af3c1aa3a9f7
bgcheng27 <69701208+bgcheng27@users.noreply.github.com>
2023-08-04 06:49:41 +0000

    Lessons 1 to 3 complete

--- start/01_01_available_books.js
+++ start/01_01_available_books.js
@@ -1,2 +1,35 @@
 // Write your code here
-console.log("hello world")
+class Book {
+  constructor(title, author, IBSN, numCopies = 10) {
+    this.title = title;
+    this.author = author;
+    this.IBSN = IBSN;
+    this.numCopies = numCopies;
+  }
+
+  get availability() {
+    return this.getAvailability();
+  }
+
+  getAvailability() {
+    if (this.numCopies <= 0) {
+      return "Out of Stock";
+    } else if (this.numCopies < 10) {
+      return "Low Stock";
+    } else {
+      return "In Stock";
+    }
+  }
+
+  sell(numSold = 1) {
+    this.numCopies -= numSold;
+  }
+
+  restock(numCopies = 5) {
+    this.numCopies += numCopies;
+  }
+}
+
+const book = new Book("Harry Potter: The Return of the King", "Frank Turek", 420)
+
+console.log(book);
--- start/01_02_movie_prototype.js
+++ start/01_02_movie_prototype.js
@@ -1,1 +1,17 @@
 // Write your code here
+class Movie {
+  constructor(title, director, genre, releaseYear, rating) {
+    this.title = title;
+    this.director = director;
+    this.genre = genre;
+    this.releaseYear = releaseYear;
+    this.rating = rating;
+  } 
+
+  getOverview() {
+    console.log(`${this.title}, a ${this.genre} film directed by ${this.director}, was released in ${this.releaseYear}. It received a rating of ${this.rating}`)
+  }
+}
+
+const endgame = new Movie("Avengers: Endgame", "Tony Stark", "Horror", 2019, 9.81)
+endgame.getOverview()
