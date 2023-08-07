f2b035c4aa6cb0f8a31ee4e384c8e17a53a08276
bgcheng27 <69701208+bgcheng27@users.noreply.github.com>
2023-08-07 03:39:22 +0000

    Lessons 13 to 15 completed

--- start/01_14_urlify.js
+++ start/01_14_urlify.js
@@ -1,1 +1,8 @@
 // Write your code here
+function urlify(string) {
+  let urlString = string.replace(/[!@#\$%\^&\*\-().,\/\\{}\+`'";:~<>]/g, "");
+  let finalString = urlString.toLowerCase().trim().replace(/\s+/g, "-");
+  return finalString;
+}
+
+console.log(urlify("--3! 420 4 CacaRrot m@gic inda-err#!#@4askdfj"));
--- start/01_15_password_strength.js
+++ start/01_15_password_strength.js
@@ -1,1 +1,24 @@
 // Write your code here
+function passwordStrength(password) {
+  let lower = password.match(/[a-z]/g);
+  let capital = password.match(/[A-Z]/g);
+  let digit = password.match(/[0-9]/g);
+  let special = password.match(/[!@#\$%\^&\*\-().,\/\\{}\+`'";:~<>]/g);
+  let long = password.length >= 8;
+
+  return lower && capital && digit && special && long ? "Your password is valid" : "Your password is Invalid"
+}
+
+console.log(passwordStrength("trollP7$"))
+
+// Feedback: positive look ahead. 0 width assertion. 
+// only returns if match or no match; does not consume whole string
+
+// . => match any character except line break
+// + => quantifier; ensures there are one or more of that character
+
+// /(?=.+[a-z])/
+// /(?=.+[A-Z])/
+// /(?=.+[0-9])/
+// /(?=.+[!@#$%^&*])/
+// /(?=.{8,})/
